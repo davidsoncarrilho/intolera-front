0fa664d16ecad837c87b0a6e062d4cfaed4c5188
Filipe Silva Oliveira <filipe.oliveira@refuturiza.com.br>
2024-06-15 17:54:30 -0300

    save

--- pages/SignUp.tsx
+++ pages/SignUp.tsx
@@ -11,6 +11,7 @@ import ProfileType from '../components/Register/ProfileType';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import "swiper/css";
 import User from '../components/Register/User';
+import Address from '../components/Register/Address';
 
 const SignUp: NextPage = () => {
   const [email, setEmail] = React.useState('');
@@ -86,6 +87,7 @@ const SignUp: NextPage = () => {
                       }}
                       allowTouchMove={false}
                       loop={false}
+                      spaceBetween={40}
                     >
                       <SwiperSlide key={0}>  
                         <ProfileType />
@@ -95,33 +97,39 @@ const SignUp: NextPage = () => {
                       </SwiperSlide>
 
                       <SwiperSlide key={1}>
-
                         <User></User>
                         <button onClick={() => swiperRef.current.slidePrev()}  
                           className='my-5 w-1/2 rounded-[4px]  px-2 py-1 text-sm font-semibold
-                           bg-[#0095f6] hover:bg-[#abddff]'>Anterior</button>
-                           <button onClick={() => swiperRef.current.slideNext()}  
+                            bg-[#0095f6] hover:bg-[#abddff]'>Anterior</button>
+                            <button onClick={() => swiperRef.current.slideNext()}  
                           className='my-5 w-1/2 rounded-[4px]  px-2 py-1 text-sm font-semibold
-                           bg-[#0095f6] hover:bg-[#abddff]'>Próximo</button>
-                        <button
-                          className={`${
-                            emailFormErrors === '' && passwordFormErrors === ''
-                              ? 'bg-[#0095f6]'
-                              : 'pointer-events-none cursor-default bg-[#abddff]'
-                          } my-5 w-full rounded-[4px]  px-2 py-1 text-sm font-semibold text-white`}
-                          type="submit"
-                        >
-                          Cadastrar
-                        </button>
-
+                            bg-[#0095f6] hover:bg-[#abddff]'>Próximo</button>
                         </SwiperSlide>
-                      <SwiperSlide key={2}>
-                        
-                        <div>
-                          asdasdsa
-                        </div>
-                    
-                      </SwiperSlide>
+
+                        <SwiperSlide key={2}>
+                          <Address></Address>
+                          <button onClick={() => swiperRef.current.slidePrev()}  
+                            className='my-5 w-1/2 rounded-[4px]  px-2 py-1 text-sm font-semibold
+                              bg-[#0095f6] hover:bg-[#abddff]'>Anterior</button>
+                              <button onClick={() => swiperRef.current.slideNext()}  
+                            className='my-5 w-1/2 rounded-[4px]  px-2 py-1 text-sm font-semibold
+                              bg-[#0095f6] hover:bg-[#abddff]'>Próximo</button>
+                        </SwiperSlide>
+
+                        <SwiperSlide key={3}>
+                          
+                            <button
+                                className={`${
+                                  emailFormErrors === '' && passwordFormErrors === ''
+                                    ? 'bg-[#0095f6]'
+                                    : 'pointer-events-none cursor-default bg-[#abddff]'
+                                } my-5 w-full rounded-[4px]  px-2 py-1 text-sm font-semibold text-white`}
+                                type="submit"
+                              >
+                                Cadastrar
+                            </button>
+                      
+                        </SwiperSlide>
                     </Swiper>
                 
                     </form> 
